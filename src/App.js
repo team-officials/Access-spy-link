5c23d392ac17780a178d93f565a954312fa67ff5
FlowPc <git config --global user.email emmykulforsure@gmail.com>
2025-04-22 17:31:53 -0700

    first update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Landingpage from './components/pages/Landingpage';
+import Landingpage from './components/Landingpage';
 
 const App = () => {
   return (
